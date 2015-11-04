abfe9dacdb652ab8f561faf03f1dcbcc6acc9647
claudiopro <claudio.procida@gmail.com>
2015-11-04 13:17:09 +0100

    Removes redundant name, solves #31

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const workshopper = require('workshopper-adventure')
     , path        = require('path')
     , learnsass   = workshopper({
-    name        : 'learn-sass'
-  , title       : 'Learn SASS'
+    title       : 'Learn SASS'
   , exerciseDir : path.join(__dirname, 'exercises')
   , appDir      : __dirname
   , languages   : ['en', 'it', 'pt-br']
