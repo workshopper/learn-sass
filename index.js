9fd5957844d4b9d904ebb950f7a5d27ee58e80a8
claudiopro <claudio.procida@gmail.com>
2015-11-04 11:17:35 +0100

    Refactored for workshopper-adventure 4.2.x

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-#!/usr/bin/env node
-
-
 const workshopper = require('workshopper-adventure')
     , path        = require('path')
-
-workshopper({
+    , learnsass   = workshopper({
     name        : 'learn-sass'
   , title       : 'Learn SASS'
   , exerciseDir : path.join(__dirname, 'exercises')
   , appDir      : __dirname
   , languages   : ['en', 'it', 'pt-br']
 })
+
+learnsass.addAll([
+  "variables",
+  "nesting",
+  "imports",
+  "mixins",
+  "mixin_content",
+  "extend_inheritance",
+  "operators",
+  "nested_properties",
+  "parent_selector",
+  "interpolation",
+  "each",
+  "for_loop",
+  "while_loop"
+])
+
+module.exports = learnsass
